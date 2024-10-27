49b26530c10c9ebd32bd2ade9b67bca443d79336
mdnahianrafi <nahian2555@gmail.com>
2024-10-27 21:19:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+  
+
+  return (
+    <>
+<Heading className={'text-2xl text-blue-700 font-bold'} text={'We #Are# Digital Marketing Agency'}/>
+    </>
+  )
+}
+
+export default App
